9f31e7f87866d984430b8e6084c49a7dd2b369e2
Saurav Paudel <Sauravpaudel22@gmail.com>
2024-02-27 14:51:39 +0545

    changes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Card = () => {
   return (
     <>
-     <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+      <div className='flex flex-row gap-4 flex-wrap justify-center items-center'>
+      <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
   <div
     className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
     <img
@@ -50,6 +51,148 @@ const Card = () => {
     </button>
   </div>
 </div> 
+
+<div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+  <div
+    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+    <img
+      src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+      alt="card-image" />
+  </div>
+  <div className="p-6">
+    <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+    Student Pilot License (SPL)
+    </h5>
+    <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+    A Student Pilot License (SPL) is a type of license that enables a student to fly a registered aircraft under the supervision or authority of a licensed instructor or be able to fly solo after receiving an endorsement from a licensed instructor.
+    </p>
+  </div>
+  <div className="p-6 pt-0">
+    <button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+      type="button">
+      Read More
+    </button>
+  </div>
+</div> 
+
+<div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+  <div
+    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+    <img
+      src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+      alt="card-image" />
+  </div>
+  <div className="p-6">
+    <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+    Student Pilot License (SPL)
+    </h5>
+    <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+    A Student Pilot License (SPL) is a type of license that enables a student to fly a registered aircraft under the supervision or authority of a licensed instructor or be able to fly solo after receiving an endorsement from a licensed instructor.
+    </p>
+  </div>
+  <div className="p-6 pt-0">
+    <button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+      type="button">
+      Read More
+    </button>
+  </div>
+</div> 
+
+<div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+  <div
+    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+    <img
+      src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+      alt="card-image" />
+  </div>
+  <div className="p-6">
+    <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+    Student Pilot License (SPL)
+    </h5>
+    <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+    A Student Pilot License (SPL) is a type of license that enables a student to fly a registered aircraft under the supervision or authority of a licensed instructor or be able to fly solo after receiving an endorsement from a licensed instructor.
+    </p>
+  </div>
+  <div className="p-6 pt-0">
+    <button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+      type="button">
+      Read More
+    </button>
+  </div>
+</div> 
+<div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+  <div
+    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+    <img
+      src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+      alt="card-image" />
+  </div>
+  <div className="p-6">
+    <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+    Student Pilot License (SPL)
+    </h5>
+    <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+    A Student Pilot License (SPL) is a type of license that enables a student to fly a registered aircraft under the supervision or authority of a licensed instructor or be able to fly solo after receiving an endorsement from a licensed instructor.
+    </p>
+  </div>
+  <div className="p-6 pt-0">
+    <button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+      type="button">
+      Read More
+    </button>
+  </div>
+</div> 
+<div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+  <div
+    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+    <img
+      src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+      alt="card-image" />
+  </div>
+  <div className="p-6">
+    <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+    Student Pilot License (SPL)
+    </h5>
+    <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+    A Student Pilot License (SPL) is a type of license that enables a student to fly a registered aircraft under the supervision or authority of a licensed instructor or be able to fly solo after receiving an endorsement from a licensed instructor.
+    </p>
+  </div>
+  <div className="p-6 pt-0">
+    <button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+      type="button">
+      Read More
+    </button>
+  </div>
+</div> 
+<div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+  <div
+    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+    <img
+      src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+      alt="card-image" />
+  </div>
+  <div className="p-6">
+    <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+    Student Pilot License (SPL)
+    </h5>
+    <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+    A Student Pilot License (SPL) is a type of license that enables a student to fly a registered aircraft under the supervision or authority of a licensed instructor or be able to fly solo after receiving an endorsement from a licensed instructor.
+    </p>
+  </div>
+  <div className="p-6 pt-0">
+    <button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+      type="button">
+      Read More
+    </button>
+  </div>
+</div> 
+      </div>
     </>
    
   )
